afde71e4ed3e52dcb4b71a2f8def5440d4ce4705
yahyaoui.ramzi <ramzi.yahyaoui89@gmail.com>
2021-01-14 02:47:50 +0100

    ahliiiiin

---
+++ projet-state/src/Compo/ramzi.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ramzi = () => {
+    return (
+        <div>
+            hello wael cava wino amer jaballah oh ya 3amer ana barcha charb 
+        </div>
+    )
+
+
+
+
+}
+export default App ;
