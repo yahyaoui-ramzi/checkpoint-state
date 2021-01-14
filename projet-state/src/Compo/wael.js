477984b2fd38ffbdb8e532dc44a249e471f1c35b
yahyaoui.ramzi <ramzi.yahyaoui89@gmail.com>
2021-01-14 19:31:56 +0100

    ramzi

--- projet-state/src/Compo/wael.js
+++ projet-state/src/Compo/wael.js
@@ -3,7 +3,7 @@ import React from 'react';
 function CA (){
     return(
         <div>
-            <h1>salut waeoul cc </h1>
+            <h1>salut waeoul cc amrouch ya maalem</h1>
         </div>
     )
 }
